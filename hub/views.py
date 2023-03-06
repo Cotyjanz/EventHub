25deeb1cbbfd74694d72e005d486b0d1582efba6
cjj <cotyjanz@goowy.com>
2023-03-05 23:10:02 -0600

    updated view queryset for index page, added root template for the event_page

--- hub/views.py
+++ hub/views.py
@@ -8,7 +8,9 @@ from hub.models import *
 # note: request object is an HttpRequestObject, it has information about
 #  request, such as the method, which can take several values including GET and POST
 def DIY_index(request):
- 	public_events = EventDetails.objects.all()
+ 	#public_events = EventDetails.objects.all()
+ 	# This query cannot be tested until the django admin is setup
+ 	public_events = EventDetails.objects.all().order_by('-event_id')[:10]
  	context = {
  		'events' : public_events
  	}
@@ -18,13 +20,13 @@ def DIY_index(request):
 # This is the view which handles urls for the event pages themselves
 def event_detail(request, primary_key):
  	# get_object_or_404 shortcut will display a 404 page if the object DNE
- 	event = get_object_or_404(EventDetails, pk=primary_key)
+ 	event = get_object_or_404(EventDetails, event_id=primary_key)
  	context = {
  		'event' : event
  	}
  	# event_page.html is the parent to all the potential templates which might
  	#   be choosen by the user
- 	return render(request, 'event_page.html', context)
+ 	return render(request, 'hub/event_page.html', context)
 
 def DIY_signup(request):
     template = loader.get_template('hub/signup.html')
