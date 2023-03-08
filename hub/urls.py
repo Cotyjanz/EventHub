be5934edd358b5d5d5aff9cea528df2174c71ccf
vencyadvani2 <126195019+vencyadvani2@users.noreply.github.com>
2023-03-08 21:48:02 +0530

    Update urls.py
    Updated urls.py to include views for register and login.html

--- hub/urls.py
+++ hub/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from hub.views import  register
+from django.conf.urls import include
+
+
 
 urlpatterns = [
+	path('accounts/', include("django.contrib.auth.urls")),
+	path('register/', register, name="register"),
 	path('', views.DIY_index, name='DIY_index'),
-    path('DIY_signup',views.DIY_signup, name ='DIY_signup'),
-    path('DIY_user_page', views.DIY_user_page, name = 'DIY_user_page'),
-    path('DIY_create', views.DIY_create, name = 'DIY_create'),
-    path('<int:primary_key>/', views.event_detail, name='event_detail')
+	path('DIY_index', views.DIY_index, name='DIY_index'),
+	path('DIY_user_page', views.DIY_user_page, name = 'DIY_user_page'),
+	path('DIY_create', views.DIY_create, name = 'DIY_create')
 ]
