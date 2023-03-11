844fcc6575cdd4e02642e9c1d389c54b588c1140
lyp479 <122639891+lyp479@users.noreply.github.com>
2023-03-11 12:03:36 +0530

    Update forms.py
    updated forms to include eventdetailsform

--- hub/forms.py
+++ hub/forms.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
 
 
-
+class EventDetailsForm(forms.ModelForm):
+    class Meta:
+        model = EventDetail
+        fields = ('title', 'description', 'Location', 'date', 'time', 'Is_public',)
+        exclude = ('u_id',)
 
 
 class RegisterForm(UserCreationForm):
