1ee4b5bf7b426e1b54ba4e1b2d1bb9bbb92f5e84
lyp479 <122639891+lyp479@users.noreply.github.com>
2023-03-11 12:02:46 +0530

    Update urls.py

--- hub/urls.py
+++ hub/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
 	path('DIY_index', views.DIY_index, name='DIY_index'),
 	path('DIY_user_page', views.DIY_user_page, name = 'DIY_user_page'),
 	path('DIY_create', views.DIY_create, name = 'DIY_create'),
+	path('layout1/', views.DIY_layout1, name = 'DIY_layout'),
 	path('<int:primary_key>/', views.event_detail, name='event_detail')
 ]
