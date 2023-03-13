0fa0afdcd7ced181096b466cf01bb2d9ddf40962
lyp479 <122639891+lyp479@users.noreply.github.com>
2023-03-13 09:11:02 +0530

    Update models.py
    u_id added for accessing user details

--- hub/models.py
+++ hub/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
    
 class EventDetails(models.Model):
    event_id = models.AutoField(primary_key= True)
+   u_id = models.ForeignKey(User, on_delete= models.CASCADE)
    title = models.CharField(max_length=255)
    description = models.TextField(max_length=255)
    Location = models.CharField(max_length=255)
