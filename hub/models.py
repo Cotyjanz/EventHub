46a688d3b7ad3476523f4ac58445ccc9707585e7
lyp479 <122639891+lyp479@users.noreply.github.com>
2023-03-16 11:52:08 +0530

    Update models.py
    Updated models.py to include poster_layout field.

--- hub/models.py
+++ hub/models.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
+#specifying the choices
+SELECT_LAYOUT = (
+         ("layout 1", "layout 1"),
+         ("layout 2", "layout 2"),
+         ("layout 3", "layout 3"),
+         ("layout 4", "layout 4"),
+         ("layout 5", "layout 5"),
+)
    
 class EventDetails(models.Model):
    event_id = models.AutoField(primary_key= True)
@@ -11,6 +18,7 @@ class EventDetails(models.Model):
    title = models.CharField(max_length=255)
    description = models.TextField(max_length=255)
    Location = models.CharField(max_length=255)
+   poster_layout = models.CharField(max_length=50, choices=SELECT_LAYOUT, default='layout 1')
    date = models.DateField()
    time = models.TimeField()
    Is_public = models.BooleanField(null= True)
