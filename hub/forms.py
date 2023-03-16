d9255a8a0c71447941031607e51ea1da6a83f56a
lyp479 <122639891+lyp479@users.noreply.github.com>
2023-03-16 19:30:21 +0530

    Update forms.py
    update forms to allow appropriate inputs for date and time.

--- hub/forms.py
+++ hub/forms.py
@@ -6,12 +6,15 @@ from hub.models import EventDetails
 class EventDetailsForm(forms.ModelForm):
     class Meta:
         model = EventDetails
-        fields = ('title', 'description', 'Location', 'date', 'time', 'Is_public','poster_layout')
+        fields = ('title', 'description', 'Location', 'date', 'time', 'Is_public', 'poster_layout','Is_rsvp',)
         widgets={
 'description': forms.TextInput(attrs={'style': 'width: 100%;', }),
-'date': forms.TextInput(attrs={'placeholder': 'YYYY-MM-DD', 'style': 'width: 40%;', }),
-'time': forms.TextInput(attrs={'placeholder': 'HH:MM:SS', 'style': 'width: 40%;', }),
-        }
+'date': forms.SelectDateWidget(attrs={'placeholder': 'YYYY-MM-DD', 'style': 'width: 10%; text-align: center;', }),
+'time': forms.TimeInput(attrs={'placeholder': 'HH:MM:SS', 'style': 'width: 30%; text-align: center;', }),
+'Is_public': forms.CheckboxInput(attrs={'style':'width:20px;height:20px; margin-left: 5%; margin-top: 2%;'}),
+'Is_rsvp': forms.CheckboxInput(attrs={'style':'width:20px;height:20px; margin-left: 5%; margin-top: 2%;'}),
+}
+
 
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
