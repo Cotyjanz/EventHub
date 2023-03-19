c56bcb173e7ff6c1ec7d562d1dbe864f3b5ae575
lyp479 <122639891+lyp479@users.noreply.github.com>
2023-03-19 12:04:28 +0530

    Update forms.py

--- hub/forms.py
+++ hub/forms.py
@@ -12,8 +12,16 @@ class EventDetailsForm(forms.ModelForm):
 'date': forms.SelectDateWidget(attrs={'placeholder': 'YYYY-MM-DD', 'style': 'width: 10%; text-align: center;', }),
 'time': forms.TimeInput(attrs={'placeholder': 'HH:MM:SS', 'style': 'width: 30%; text-align: center;', }),
 'Is_public': forms.CheckboxInput(attrs={'style':'width:20px;height:20px; margin-left: 5%; margin-top: 2%;'}),
-'Is_rsvp': forms.CheckboxInput(attrs={'style':'width:20px;height:20px; margin-left: 5%; margin-top: 2%;'}),
 }
+
+class EventRSVPForm(forms.ModelForm):
+    class Meta:
+        model = EventDetails
+        fields = ('Is_rsvp',)
+        widgets={
+'Is_rsvp': forms.NumberInput(attrs={'placeholder': 'No. of Guests','style':'border-radius: 10px;font-size: 11px ;width: 80px;height:35px; '}),
+}
+        
 
 
 class RegisterForm(UserCreationForm):
