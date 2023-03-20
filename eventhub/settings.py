d61ae31f50854c8f8b0fe0f47ed4b83c6f0a553b
Schmoozer <Cotyjanz@users.noreply.github.com>
2023-03-20 02:24:20 -0600

    Update settings.py

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =
+SECRET_KEY = 'django-insecure-px$#nkf&rl9_lus+9&u)(_i)&wi&3%z58ria!=mz1+zv#^bwg@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
